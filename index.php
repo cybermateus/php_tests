f2d87fa0a364d23410cd5e56e9534a10a5d64839
cybermateus <45902870+cybermateus@users.noreply.github.com>
2018-12-15 21:47:24 +0100

    Update index.php

--- index.php
+++ index.php
@@ -13,3 +13,17 @@ $user = $_SESSION['user'];
 echo "Hello " . $user;
 
 ?>
+
+<form method="post" action="index.php">
+    <button type="submit" name="logout"> logout </button>
+</form>
+
+<?php 
+
+if (isset($_POST['logout'])){
+    session_unset();
+    header ("location: index.php")
+}
+
+?>
+
